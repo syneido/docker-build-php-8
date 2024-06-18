3e18e237c2c6097a8c34e2187f2d672dbd59928e
adefilippi <anthony.defilippi@gmail.com>
2024-06-18 08:52:38 +0200

    Update php version to 8.3

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # docker-build-php-8
+
+
+
+## Local tests
+###### build-php
+docker buildx build  \
+--build-arg PHP_VERSION=8.3 \
+--build-arg ALPINE_VERSION=3.20 \
+--file ./Dockerfile-build \
+--tag ghcr.io/syneido/build-php8.3:latest --target build \
+./
+###### php-fpm
+docker buildx build  \
+--build-arg PHP_VERSION=8.3 \
+--build-arg ALPINE_VERSION=3.20 \
+--file ./Dockerfile-fpm \
+--tag ghcr.io/syneido/prod-php8.3-fpm:latest --target prod \
+./
+
+
+###### build-php-sqlsrv
+docker buildx build  \
+--build-arg PHP_VERSION=8.3 \
+--build-arg ALPINE_VERSION=3.20 \
+--file ./Dockerfile-build \
+--tag ghcr.io/syneido/build-php8.3:latest --target build-sqlsrv \
+./
+
+
+###### php-fpm
+docker buildx build  \
+--build-arg PHP_VERSION=8.3 \
+--build-arg ALPINE_VERSION=3.20 \
+--file ./Dockerfile-fpm \
+--tag ghcr.io/syneido/prod-php8.3-fpm:latest --target prod-sqlsrv \
+./
--- configure-sqlsrv.sh
+++ configure-sqlsrv.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
-
 set -ex
+
+case $(uname -m) in
+    x86_64)   architecture="amd64" ;;
+    arm64)   architecture="arm64" ;;
+    *) architecture="unsupported" ;;
+esac
+if [[ "unsupported" == "$architecture" ]];
+then
+    echo "Alpine architecture $(uname -m) is not currently supported.";
+    exit;
+fi
+
 
 apk update
 
 apk add --no-cache --virtual .build-deps $PHPIZE_DEPS unixodbc-dev
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/msodbcsql17_17.6.1.1-1_amd64.apk
-curl -O https://download.microsoft.com/download/e/4/e/e4e67866-dffd-428c-aac7-8d28ddafb39b/mssql-tools_17.6.1.1-1_amd64.apk
+curl -O https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/msodbcsql18_18.3.3.1-1_$architecture.apk
+curl -O https://download.microsoft.com/download/3/5/5/355d7943-a338-41a7-858d-53b259ea33f5/mssql-tools18_18.3.1.1-1_$architecture.apk
 
-apk add --allow-untrusted msodbcsql17_17.6.1.1-1_amd64.apk
-apk add --allow-untrusted mssql-tools_17.6.1.1-1_amd64.apk
+apk add --allow-untrusted msodbcsql18_18.3.3.1-1_$architecture.apk
+apk add --allow-untrusted mssql-tools18_18.3.1.1-1_$architecture.apk
 
 pecl install sqlsrv pdo_sqlsrv
 docker-php-ext-enable sqlsrv pdo_sqlsrv
